fa03701ffcfc290848ec1b637c1b9ae4d01d9f7f
erictmc <etmcmillen@gmail.com>
2019-01-02 23:22:24 -0800

    More exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-export * from "./constants";
-export * from "./geo";
-export * from "./places";
-export * from "./time";
+export {
+  COFFEE_AND_SNACKS_CATEGORY,
+  OUTDOORS_CATEGORY,
+  OUT_AND_ABOUT_CATEGORY,
+  EAT_CATEGORY,
+  NIGHTLIFE_CATEGORY,
+  SHOP_CATEGORY,
+  PHOTO_OP_CATEGORY
+} from "./constants";
+
+export {
+  getDistanceFromLatLonInMiles
+} from "./geo";
+
+export {
+  defineCategory
+} from "./places";
+
+export {
+  openAfterCutoff,
+  parseClockTime,
+  InvalidTimeError
+} from "./time";
 
 export {
   convertObjToCamelCase,
